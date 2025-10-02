b6fe1ae53860693bf0e540db81128f83e9cbf1da
abl <0pa.p1drila@gmail.com>
2025-10-02 16:21:00 +0600

    213

--- pythonwebproject/main.py
+++ pythonwebproject/main.py
@@ -1,1 +1,14 @@
+import webview
+import os
+import subprocess
 
+def run():
+    out = subprocess.check_output("ls", shell=True, text=True)
+    return out
+
+if __name__ == "__main__":
+    file_path = os.path.abspath("web/index.html")
+    api = Api()
+    window = webview.create_window("web", file_path, js_api=api)
+    webview.start()
+    main()
